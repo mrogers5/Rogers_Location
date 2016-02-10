c154e67e5ec51d0d08708f71d1d3df5c1c497b44
mrogers5 <melanierogers@d-10-16-12-5.dhcp4.washington.edu>
2016-02-09 18:16:11 -0800

    Initial Commit

---
+++ Rogers_Location/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rogers_Location
+//
+//  Created by Melanie Rogers on 2/9/16.
+//  Copyright © 2016 mrogers5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
